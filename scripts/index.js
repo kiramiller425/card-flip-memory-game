7b5cc30226941f66bbfbb179741ec92066f1aabc
Kira Miller <kira.miller2011@gmail.com>
2019-07-20 13:44:12 -0700

    added in more to rough draft

--- scripts/index.js
+++ scripts/index.js
@@ -1,126 +1,223 @@
 /**
  * This document contains the JS / jQuery for the Card Flip Memory Match Game. Comments are in JSDoc form.
  * Here are some of the articles I read while making this game:
- https://hackernoon.com/object-oriented-programming-in-vanilla-javascript-f3945b15f08a
- https://addyosmani.com/blog/essential-js-namespacing/#beginners
- https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s04.html
+ *    https://hackernoon.com/object-oriented-programming-in-vanilla-javascript-f3945b15f08a
+ *    https://addyosmani.com/blog/essential-js-namespacing/#beginners
+ *    https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s04.html
+ *    https://devdocs.io/jsdoc/
+ *    https://devhints.io/jsdoc
+ *
  * @author: Kira Miller
  */
  
 ;(function($) {
 
 	/**
-	 * My namespace for this app. This checks for conflicting namespace names and will use the one that already exists if there is a conflict. See option 4 (best practice) of https://addyosmani.com/blog/essential-js-namespacing/#beginners I'm using the jQuery namespace pattern. See: https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s04.html
+	 * My namespace for this app. This checks for conflicting namespace names and will use the one that already exists if there is a conflict. See option 4 (best practice) of https://addyosmani.com/blog/essential-js-namespacing/#beginners I'm using the jQuery namespace pattern. See: https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s04.html I've also set up the object literal here.
+	 *
 	 * @namespace
 	 */
-	$.KirasMatchGameNamespace || ($.KirasMatchGameNamespace = {});
-
-	/**
-	 * Nested namespace for setting up and starting this app. This checks for conflicting namespace names and will use the one that already exists if there is a conflict. See option 4 (best practice) of https://addyosmani.com/blog/essential-js-namespacing/#beginners I'm using the jQuery namespace pattern. See: https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s04.html
-	 * @namespace
-	 */
-	$.KirasMatchGameNamespace.SetupAndStartGame = function(gameBoardElement, relativeUrisOfUniquePicturesArray) {
+	$.KirasCardMatchMemoryGame || ($.KirasCardMatchMemoryGame = {
 		
-		console.log("Setting up game board.");
+		/**
+		 * This sets up the game for the first time after the page loads.
+		 *
+		 * @param {string} gameBoardElementId is the id name of the element on the HTML page which this function will set up the game elements into.
+		 * @param {string[]} relativeUrisOfUniquePicturesArray is the array containing all unique card images. The first one is the back of each card.
+		 */
+		setupGame: function(gameBoardElementId, relativeUrisOfUniquePicturesArray) {
+			// Log that we are setting up the game:
+			console.log("Setting up the game.");
 			
-		var numberOfCards = 0;
-		var hiddenCardUriPicture = "";
+			// todo: hide the gameboard for now until we're done
+			// display: none / in jquery
+				
+			// Add these variables to the game, and set up with defaults for now:
+			$.KirasCardMatchMemoryGame.backOfCardPictureUri = "";
+			$.KirasCardMatchMemoryGame.hasOneCardBeenClicked = false;
+			$.KirasCardMatchMemoryGame.hasSetupError = true; // Leave it as true in case this function returns due to a setup error. This will change to false at the end of this function.
+			$.KirasCardMatchMemoryGame.idOfPreviousClickedCard = "";
+			$.KirasCardMatchMemoryGame.isGameOver = false;
+			$.KirasCardMatchMemoryGame.maxPointsGivenPerCorrectMatch = 1000;
+			$.KirasCardMatchMemoryGame.numberOfCards = 0;
+			$.KirasCardMatchMemoryGame.numberOfUniquePictures = 0;
+			$.KirasCardMatchMemoryGame.pointsToBeAwardedAfterNextMatchFound = $.KirasCardMatchMemoryGame.maxPointsGivenPerCorrectMatch; // Game subtracts points from this for each mismatch until match is found.
+			$.KirasCardMatchMemoryGame.shouldIgnoreAnyClicks = true; // Ignore any user input right now until we're done setting up the game.
+			$.KirasCardMatchMemoryGame.totalPointsScoredSoFar = 0;
+						
+			// Arrays:
+			$.KirasCardMatchMemoryGame.allCardValues;
+			$.KirasCardMatchMemoryGame.allFaceUpPictureUris = relativeUrisOfUniquePicturesArray;
 			
-		// Setup access to the jQuery version of this game board element:
-		context.$gameBoardElement = $(gameBoardElement);
-		
-		// Setup access to the DOM version of this game board element:
-		context.gameBoardElement = gameBoardElement;
+			// These variables will hold jQuery elements eventually:
+			$.KirasCardMatchMemoryGame.$cardsContainerElement;
+			$.KirasCardMatchMemoryGame.$gameBoardElement;
+			$.KirasCardMatchMemoryGame.$matchCountContainerElement;
+			$.KirasCardMatchMemoryGame.$messageContainerElement;
+			$.KirasCardMatchMemoryGame.$pointsContainerElement;
+			$.KirasCardMatchMemoryGame.$statsDashboardElement;
+			
+			// First do some validation of the inputs:
+			
+			// If the gameBoardElementId is null or undefined:
+			if(gameBoardElementId === undefined || gameBoardElementId === null) {
+				// Log this error:
+				console.error("The game board element id was " + gameBoardElementId + ". Cannot start game.");
+				
+				return;
+			}
+			
+			// Setup access to the jQuery version of this game board element:
+			$.KirasCardMatchMemoryGame.$gameBoardElement = $("#" + gameBoardElementId);
 
-		// Add a reverse reference to the DOM object:
-		context.$gameBoardElement.data("KirasMatchGameNamespace", context);
+			// If the element doesn't exist:
+			if($.KirasCardMatchMemoryGame.$gameBoardElement === undefined || $.KirasCardMatchMemoryGame.$gameBoardElement === null) {
+				// Log this error:
+				console.error("The game board element doesn't exist on the page. Cannot start game.");
+				
+				return;
+			}
+			
+			// Add a reverse reference to the DOM object:
+			$.KirasCardMatchMemoryGame.$gameBoardElement.data("KirasCardMatchMemoryGame", $.KirasCardMatchMemoryGame);
+			
+			// If the pictures array is null or undefined:
+			if(relativeUrisOfUniquePicturesArray === undefined || relativeUrisOfUniquePicturesArray === null) {
+				// Log this error:
+				console.error("The pictures array was " + relativeUrisOfUniquePicturesArray + ". Cannot start game.");
+				
+				return;
+			}
+			
+			// If the number of pictures is less than 2, then we cannot have a game:
+			if(relativeUrisOfUniquePicturesArray.length < 2) {
+				// Log this error:
+				console.error("There were too few pictures in the array. Cannot start game.");
+								
+				return;
+			}
+			
+			// Log that the input parameters passed validation:
+			console.log("Input parameters passed validation.");
+			
+			// Set the number of unique pictures (minus the back-of-card picture):
+			$.KirasCardMatchMemoryGame.numberOfUniquePictures = relativeUrisOfUniquePicturesArray.length - 1;
+
+			// Set the total number of cards:
+			$.KirasCardMatchMemoryGame.numberOfCards = $.KirasCardMatchMemoryGame.numberOfUniquePictures * 2;
+
+			// Add the stats dashboard (with points, match count, and message containers) and cards container to the page:
+			$.KirasCardMatchMemoryGame.$gameBoardElement.html("<div id='statsDashboard'>"
+				+ 	"<div id='pointsContainer'></div>"
+				+ 	"<div id='matchCountContainer'></div>"
+				+ 	"<div id='messageContainer'></div>"
+				+ "</div>"
+				+ "<div id='cardsContainer'></div>"
+				);
+			
+			// Store these jQuery objects for future reference:
+			$.KirasCardMatchMemoryGame.$statsDashboardElement = $.KirasCardMatchMemoryGame.$gameBoardElement.find("#statsDashboard");
+			$.KirasCardMatchMemoryGame.$pointsContainerElement = $.KirasCardMatchMemoryGame.$gameBoardElement.find("#pointsContainer");
+			$.KirasCardMatchMemoryGame.$matchCountContainerElement = $.KirasCardMatchMemoryGame.$gameBoardElement.find("#matchCountContainer");
+			$.KirasCardMatchMemoryGame.$messageContainerElement = $.KirasCardMatchMemoryGame.$gameBoardElement.find("#messageContainer");
+			$.KirasCardMatchMemoryGame.$cardsContainerElement = $.KirasCardMatchMemoryGame.$gameBoardElement.find("#cardsContainer");
+
+			// Add all cards to the page, within the cards container:
+			var cardsHTML = "";
+			for(var i = 0; i < $.KirasCardMatchMemoryGame.numberOfCards; i++) {
+				// The first URI in the array is the back of the card:
+				cardsHTML += "<div id='cardId" + i + "' class='gameCard' onclick='$.KirasCardMatchMemoryGame.cardClicked("+ i + ")'><img src='" + relativeUrisOfUniquePicturesArray[0] + "' /></div>";
+			}
+			$.KirasCardMatchMemoryGame.$cardsContainerElement.html(cardsHTML);
+
+			// Set up the array which will contain all the cards' values:
+			$.KirasCardMatchMemoryGame.allCardValues = new Array($.KirasCardMatchMemoryGame.numberOfCards);
+
+			// When it gets to this point, it means no errors were encountered:
+			$.KirasCardMatchMemoryGame.hasSetupError = false;
+
+			// Log that we are done setting up the game:
+			console.log("Finished setting up the game.");
+			
+			// Reset (to shuffle cards and set up messages) to start the game:
+			$.KirasCardMatchMemoryGame.resetGame();			
+		},
 		
-		if(relativeUrisOfUniquePicturesArray === undefined || relativeUrisOfUniquePicturesArray === null) {
-			// TODO: test this:
-			console.error("The pictures array was undefined. Cannot start game.");
-			return;
+		/**
+		 * This resets the game; shuffles the cards, sets the points accumulated to 0, resets the messages, and re/starts the game.
+		 */
+		resetGame: function() {
+			// Log that we are resetting the game:
+			console.log("Resetting the game.");
+			
+			// todo: hide gameboard
+			
+			// If there was a problem during setup:
+			if($.KirasCardMatchMemoryGame.hasSetupError) {
+				// Log the error and quit the game:
+				console.error("Cannot start game due to a setup error.");
+				
+				// Display an error to the user:
+				$.KirasCardMatchMemoryGame.$messageContainerElement.html("Sorry, something went wrong. Please go back and try again.");
+				
+				
+			// todo: show gameboard
+				
+				return;
+			}
+			
+			// Reset points to 0:
+			$.KirasCardMatchMemoryGame.totalPointsScoredSoFar = 0;
+			
+			// Reset display to user:
+			$.KirasCardMatchMemoryGame.$pointsContainerElement.html("Total points: " + $.KirasCardMatchMemoryGame.totalPointsScoredSoFar);
+			
+			// Set initial match count display:
+			
+			// set initial message:
+			
+			// Set all cards to their face-down display:
+			
+			// shuffle cards (randomly assign values)
+			var countsOfFaceValuesUsed = [];
+			for(var i = 0; i < $.KirasCardMatchMemoryGame.numberOfCards; i++) {
+			}
+			
+			//$.KirasCardMatchMemoryGame.allFaceUpPictureUris
+			//$.KirasCardMatchMemoryGame.allCardValues
+			
+			
+			// todo: show gameboard to user
+			
+			// Log that we are done resetting the game:
+			console.log("Finished resetting the game. Game has started.");
+		},
+		
+		/**
+		 * This handles when a card is clicked on.
+		 *
+		 * @param {number} clickedCardId is the id of the card which was clicked on.
+		 */
+		cardClicked: function(clickedCardId) {
+			
+			// TODO: replace this with actual content:
+			console.log("hi from card number " + clickedCardId);
 		}
 		
-		var numberOfUniquePictures = relativeUrisOfUniquePicturesArray.length;
 		
-		console.log(numberOfUniquePictures);
-		
-		
-		if(numberOfUniquePictures < 2) {
-			// TODO: test this:
-			console.log("uh oh");
-			return;
-		}
-			
-			hiddenCardUriPicture
-			
-			context.relativeUrisOfUniquePicturesArray = relativeUrisOfUniquePicturesArray;
-			
-			// set up and add the cards to DOM:
-			var htmlForCards = "<div id='cardsContainer'>";
-			htmlForCards += "<div><img id='' src='' onClick='' /></div>";
-			$("#gameBoardContainer").html(htmlForCards);
-			
-			
-			
-		};
-		
-	};  
-
-
-	$.KirasMatchGameNamespace.MemoryMatchGame = function (gameBoardElement, relativeUriOfBackPicture, relativeUrisOfUniqueFacePictures) {
+	});
 	
-		// To avoid scope issues, use "base" instead of "this" to reference this class from internal events and functions.
-        var base = this;
-
-		// Setup access to the jQuery version of this game board element:
-        base.$gameBoardElement = $(gameBoardElement);
-		
-		// Setup access to the DOM version of this game board element:
-        base.gameBoardElement = gameBoardElement;
-
-        // Add a reverse reference to the DOM object:
-        base.$gameBoardElement.data("KirasMatchGameNamespace.MemoryMatchGame", base);
-
-		// Setup an initialization function:
-        base.init = function () {
-			
-			
-			
-			
-            // Put our initialization code here
-			
-			// set up and add the cards to DOM:
-			var htmlForCards = "<div id='cardsContainer'>";
-			htmlForCards += "<div><img id='' src='' onClick='' /></div>";
-			$("#gameBoardContainer").html(htmlForCards);
-			
-			
-			
-			
-			
-        };
-
-        // Sample Function, Uncomment to use
-        // base.functionName = function( parameters ){
-        // 
-        // };
-		
-        // Run the initializer to setup the game:
-        base.init();
-	};
+	// temporary array for testing:
+	var relativeUrisOfUniquePicturesArray = [ "images/backOfCard.svg", "images/appleGreen.svg", "images/cherriesRed.svg", "images/peachOrange.svg", "images/pearYellow.svg", "images/plumPurple.svg" ];
 	
-    $.KirasMatchGameNamespace.MemoryMatchGame.defaultSetupKeyValuePairs = {
-        picturesJsonFile: "scripts/pictures.json"
-    };
+	// Setup and start the game:
+	$.KirasCardMatchMemoryGame.setupGame("gameBoardContainer", relativeUrisOfUniquePicturesArray);
+	
+	
+	
+	
+	
 
-    $.fn.KirasMatchGameNamespace_MemoryMatchGame = function(gameSetupObjectLiterals, gameSetupValues) {
-		console.log('hi');
-        return this.each(function() {
-			console.log(gameSetupObjectLiterals);
-            (new $.KirasMatchGameNamespace.MemoryMatchGame(this, gameSetupObjectLiterals, gameSetupValues));
-        });
-    };
 	 
 	 
 	/**
@@ -300,10 +397,6 @@ var _prototypeProperties = function (child, staticProps, instanceProps) { if (st
 	// Setup and start the game:
 	//startGame();
 
-	// Set the element with this ID as the gameboard. Use this json file for the pictures. Setup and start the game:
-	$("#gameBoardContainer").KirasMatchGameNamespace_MemoryMatchGame({
-		picturesJsonFile: "scripts/pictures.json"
-	});
 	
 	// TODO:
 	// Maybe make a generic class that returns an array of picture urls
